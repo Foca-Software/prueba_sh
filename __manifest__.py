b58ba1236d55e1739f9ec14dbdc3eb7da2c900a4
Jerónimo Alvarez <96080783+jeroalvarez1@users.noreply.github.com>
2025-10-30 15:48:35 -0300

    Create __manifest__.py for Odoo module
    Initial manifest file for Odoo module with basic metadata.

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Prueba SH',
+    'version': '1.0.0',
+    'summary': 'Módulo de prueba genérico',
+    'description': """
+Módulo de ejemplo básico para Odoo 18.
+Ideal como plantilla inicial para desarrollo o pruebas.
+    """,
+    'author': 'Tu Nombre o Empresa',
+    'website': 'https://www.tuempresa.com',
+    'category': 'Uncategorized',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
