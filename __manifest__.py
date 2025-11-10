bea4f65c930e8cdf8e64dc361c6e7c7ec9656caf
Jerónimo Alvarez <96080783+jeroalvarez1@users.noreply.github.com>
2025-11-10 13:35:33 -0300

    Bump version from 1.0.0 to 1.0.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Prueba SH',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'summary': 'Módulo de prueba genérico',
     'description': """
 Módulo de ejemplo básico para Odoo 18.
